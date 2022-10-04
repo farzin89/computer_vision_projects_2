9c2a651c4b96bee532f9ae979d7ca6ddc049ddb3
farzin89 <farzin_ahmadi90@yahoo.com>
2022-10-04 09:22:13 +0200

    create function for mediapipe function

--- face_detection/face_detection_module.py
+++ face_detection/face_detection_module.py
@@ -1,7 +1,15 @@
 import time
-
 import cv2
-import mediapipe
+import mediapipe as mp
+
+class FaceDetectior():
+    def __init__(self,minDetectionCon=0.5):
+
+        self.minDetectionCon = minDetectionCon
+        self.mpFaceDetection = mp.solutions.face_detection
+        self.moDraw = mp.solutions.drawing_utils
+        self.mpFaceDetection = self.mpFaceDetection.FaceDetection(0.75)
+
 
 
 
