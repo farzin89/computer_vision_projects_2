e1dc08e88e0f2f82fbdb8ded30351363d9ded09a
farzin89 <farzin_ahmadi90@yahoo.com>
2022-10-04 09:51:21 +0200

    create function to find faces and create rectangle

--- face_detection/face_detection_module.py
+++ face_detection/face_detection_module.py
@@ -7,8 +7,24 @@ class FaceDetectior():
 
         self.minDetectionCon = minDetectionCon
         self.mpFaceDetection = mp.solutions.face_detection
-        self.moDraw = mp.solutions.drawing_utils
-        self.mpFaceDetection = self.mpFaceDetection.FaceDetection(0.75)
+        self.mpDraw = mp.solutions.drawing_utils
+        self.faceDetection = self.mpFaceDetection.FaceDetection(0.75)
+
+    def findFaces(self,img,draw = True):
+        imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+        self.results = self.faceDetection.process(imgRGB)
+        #print(self.results)
+
+        if self.results.detections:
+            for id,detection in enumerate(self.results.detections):
+                bboxC = detection.location_data.relative_bounding_box
+                ih,iw,ic = img.shape
+                bbox = int(bboxC.xmin * iw),int(bboxC.ymin * ih),\
+                       int(bboxC.width * iw), int(bboxC.height * ih)
+                cv2.rectangle(img,bbox,(255,0,255),2)
+                cv2.putText(img,f'{int(detection.score[0] * 100)}%',
+                            (bbox[0],bbox[1]-20),cv2.FONT_HERSHEY_PLAIN,2,(255,0,255),2)
+
 
 
 
