b646f746835ea36cd9c609fd68a3819a554ee6e4
farzin89 <farzin_ahmadi90@yahoo.com>
2022-10-04 10:28:17 +0200

    add fancy draw on rectangle

--- face_detection/face_detection_module.py
+++ face_detection/face_detection_module.py
@@ -23,17 +23,34 @@ class FaceDetectior():
                 bbox = int(bboxC.xmin * iw),int(bboxC.ymin * ih),\
                        int(bboxC.width * iw), int(bboxC.height * ih)
                 bboxs.append([bbox,detection.score])
-                cv2.rectangle(img,bbox,(255,0,255),2)
-                cv2.putText(img,f'{int(detection.score[0] * 100)}%',
+                #cv2.rectangle(img,bbox,(255,0,255),2)
+                if draw :
+                    img= self.fancyDraw(img,bbox)
+                    cv2.putText(img,f'{int(detection.score[0] * 100)}%',
                             (bbox[0],bbox[1]-20),cv2.FONT_HERSHEY_PLAIN,2,(255,0,255),2)
 
         return img,bboxs
 
+    def fancyDraw(self,img,bbox,l=30,t=5,rt = 1):
+        x,y,w,h = bbox
+        x1,y1 = x + w, y + h
+
+        cv2.rectangle(img, bbox, (255, 0, 255), rt)
+        # top left x,y
+        cv2.line(img,(x,y),(x+l,y),(255,0,255),t)
+        cv2.line(img,(x, y), (x,y+l), (255, 0, 255), t)
+        # Top Right x1,y
+        cv2.line(img, (x1, y), (x1 - l, y), (255, 0, 255), t)
+        cv2.line(img, (x1, y), (x1, y + l), (255, 0, 255), t)
+        # Bottom left x,y1
+        cv2.line(img,(x,y1),(x+l,y1),(255,0,255),t)
+        cv2.line(img,(x, y1), (x,y1-l), (255, 0, 255), t)
+        # bottom Right x1,y1
+        cv2.line(img, (x1, y1), (x1 - l, y1), (255, 0, 255), t)
+        cv2.line(img, (x1, y1), (x1, y1 - l), (255, 0, 255), t)
 
 
-
-
-
+        return img
 
 
 
