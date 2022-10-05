1941244527d9b0bb8767c5dc330cdca1198c6206
farzin89 <farzin_ahmadi90@yahoo.com>
2022-10-05 07:53:11 +0200

    calculate the frame rate and show on video

--- face_mesh/face_mesh.py
+++ face_mesh/face_mesh.py
@@ -4,11 +4,15 @@ import mediapipe as mp
 import time
 
 cap = cv2.VideoCapture(0)
-
+pTime = 0
 while True :
     success,img = cap.read()
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+    cv2.putText(img,f'FPS: {int(fps)}',(20,70),cv2.FONT_HERSHEY_PLAIN,3,(0,255,0),3)
     cv2.imshow("Image",img)
     cv2.waitKey(1)
-    
+
 
 
