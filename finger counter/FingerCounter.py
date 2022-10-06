d11aa91cc7e64c8c1b84bb49e75ee1795bd99d54
farzin89 <farzin_ahmadi90@yahoo.com>
2022-10-06 13:01:33 +0200

    how to overlay finger image on original image

--- finger counter/FingerCounter.py
+++ finger counter/FingerCounter.py
@@ -9,9 +9,22 @@ cap.set(4,hCam)
 
 folderPath = "fingerImages"
 myList = os.listdir(folderPath)
+
 print(myList)
+overlayList =[]
+for imPath in myList:
+    image = cv2.imread(f'{folderPath}/{imPath}')
+   # print((f'{folderPath}/{imPath}'))
+    overlayList.append(image)
+print(len(overlayList))
+
+
 while True:
     success,img = cap.read()
+    h,w,c = overlayList[0].shape
+    img[0:h,0:w]=overlayList[0]
+
+    
 
     cv2.imshow("image",img)
     cv2.waitKey(1)
