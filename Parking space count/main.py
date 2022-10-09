03240b3ec7ace19a31ba016a353bcdf9ceee7bfe
farzin89 <farzin_ahmadi90@yahoo.com>
2022-10-09 13:08:19 +0200

    for repeat the video

--- Parking space count/main.py
+++ Parking space count/main.py
@@ -7,6 +7,12 @@ import numpy as np
 cap = cv2.VideoCapture('C:\Parking Sapace Video\carPark.mp4')
 
 while True :
+
+    # in order to loop video
+    if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
+       cap.set(cv2.CAP_PROP_POS_FRAMES,0)
+
+
     success,img = cap.read()
     cv2.imshow("Image",img)
-    cv2.waitKey(10)
+    cv2.waitKey(1)
