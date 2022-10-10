f6abedd97de4fdb63a0d0d44b113167d19efed79
farzin89 <farzin_ahmadi90@yahoo.com>
2022-10-10 07:49:34 +0200

    convert to gray and make Blur

--- Parking space count/main.py
+++ Parking space count/main.py
@@ -17,7 +17,7 @@ def checkParkingSpace():
 
         imgCrop = img[y:y+height,x:x+width]
         cv2.imshow(str(x*y),imgCrop)
-        cv2.rectangle(img, pos, (pos[0] + width, pos[1] + height), (255, 0, 255), 2)
+
 
 while True :
 
@@ -25,10 +25,17 @@ while True :
     if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
        cap.set(cv2.CAP_PROP_POS_FRAMES,0)
     success, img = cap.read()
+
+    imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    imgBlur = cv2.GaussianBlur(imgGray,(3,3),1)
+
     checkParkingSpace()
+    for pos in posList:
+        cv2.rectangle(img, pos, (pos[0] + width, pos[1] + height), (255, 0, 255), 2)
 
 
     cv2.imshow("Image",img)
+    cv2.imshow("ImageBlur", imgBlur)
     cv2.waitKey(1)
 
 # is car inside the rectangle or not
